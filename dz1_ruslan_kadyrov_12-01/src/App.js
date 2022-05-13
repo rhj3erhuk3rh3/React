b84a627b126ed8b985a40f0e6837ae078bdef8e6
rhj3erhuk3rh3 <ruslanaskar361@gmail.com>
2022-05-13 18:16:52 +0600

    go

---
+++ dz1_ruslan_kadyrov_12-01/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./footer";
+import Header from "./header";
+import './App.css'
+
+function App() {
+  return (
+      <div className='wrapper'>
+          <Header/>
+          <Footer/>
+      </div>
+  );
+}
+
+export default App;
